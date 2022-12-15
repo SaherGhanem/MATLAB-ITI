5187e55b9895f6d1c44670798a844aef6106ae24
Saher Ghanem <saherghanem333@gmail.com>
2022-12-15 15:38:43 +0200

    WELCOME_MATLAB

---
+++ WELCOME MATLAB/WELCOME MATLAB.m
@@ -0,0 +1,38 @@
+%bitand
+num1= uint8(5);
+num2 = uint8(7);
+
+%LogicalAnd=num1&num2;
+%BitAnd=bitand(a,b);
+%BitAnd=bitand(num1,num2,'uint8')
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+%BitOr=bitor(num1,num2,'uint8')
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% Extractedbit = 1;
+% bitvalue= bitget(1,Extractedbit+1);
+% disp(bitvalue);
+
+
+%Multiple bits set
+% Byte = uint8(0);
+% Required
+
+
+%bitshift
+Var=5;
+%Right Shift/2 - NUM
+%Left shift?*2+Num
+%AfterShift=bitshift(5,1); %1\+ve => Left Shift ,and LSBs become 
+
+
+
+MyVar1= uint32(1);
+disp(swapbytes(MyVar1))
+
+
+
+
+
